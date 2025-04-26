fc7cb090f3e27208e5ff651c5c9eea0c09fe27d2
Camillalalala <camilla4clubs@gmail.com>
2025-04-26 13:42:06 -0700

    Deleted extra home.js page

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,6 @@ export default function Home() {
         <p className="text-lg text-gray-600">
           This is your default home page with a navbar.
         </p>
-        <Map></Map>
       </main>
     </>
   );
