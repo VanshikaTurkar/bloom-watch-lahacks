e308286117869c359c5d65795628488a2f4de1d2
Lian Elsa Linton <64187585+lianlinton@users.noreply.github.com>
2025-04-27 03:54:06 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 "use client";
 import dynamic from "next/dynamic";
 
-// src/app/page.js
 import Navbar from "../app/components/Navbar";
-import HomePage from "../app/pages/HomePage"; // <-- You need to import HomePage!
+import HomePage from "../app/pages/HomePage"; 
 
-// Dynamic import disables SSR for this component
 const Map = dynamic(() => import("./map/Map"), { ssr: false });
 
 export default function Home() {
