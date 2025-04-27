8cc8e8456d766a0548e2b2f15a2a30bbae7aba10
Vanshika Turkar <vanshika@Vanshikas-MacBook-Air.local>
2025-04-26 20:21:47 -0700

    report backend and frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
 
-// Dynamic import disables SSR for this component
-const Form = dynamic(() => import("./report/FormSubmit"), {ssr:false});
-const Alert = dynamic(() => import("./report/Alert"), {ssr:false});
-
 // src/app/page.js
 import Navbar from "../app/components/Navbar";
 import HomePage from "../app/pages/HomePage"; // <-- You need to import HomePage!
@@ -21,9 +17,6 @@ export default function Home() {
         <p className="text-lg text-gray-600">
           This is your default home page with a navbar.
         </p>
-        <Form></Form>
-        <Map></Map>
-        <Alert></Alert>
       </main>
     </>
   );
